df1ef50e24bb1d808e036eea1d8231868f2afe2f
dazmag <iscope09@hotmail.com>
2020-09-28 00:11:42 -0400

    words array

--- app.js
+++ app.js
@@ -5,3 +5,34 @@ Part 1
 -MUST be able to count the words 
 -Must be able to display how much words someone i type per minute
 */
+
+// List of Animals
+const words = ['dog', 'rabbit', 'cat', 'shark', 'seahorse', 'turtle',
+    'dolphin', 'tiger', 'bear', 'Penguin', 'ladybug', 'elephant','giraffe', 'butterfly',
+    'spider', 'dinosaur', 'fox', 'hamster', 'koala', 'anteater'
+]
+
+// List of Moons
+const moonWords = ['earth', 'jupiter', 'saturn', 'neptune', 'mars', 'venus', 
+'uranus', 'mercury', 'pluto']
+
+//List of fruits and veggies
+const fvWords = [ 'banana', 'carrot', 'spinach', 'orange', 'mango', 'grapes', 'strawberry', 
+    'apple', 'blueberry', 'pineapple', 'watermelon', 'nectarine', 'pomegranate'
+    'cabbage', 'cucumber', 'potato', 'ginger', 'avocado', 'radish',
+    'mushroom', 'pepper', 'pumpkin'
+]
+// Variable 
+
+
+const word = document.getElementById('word');
+const text = document.getElementById('text');
+const score = document.getElementById('score');
+const time = document.getElementById('time');
+const endGame = document.getElementById('end-game');
+const settingsBtn = document.getElementById('settings-btn');
+const settings = document.getElementById('settings');
+const form = document.getElementById('form');
+const diffSelect = document.getElementById('Difficulty');
+
+
