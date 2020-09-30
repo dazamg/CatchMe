f70713f83f72e0cae4a7294dea75fa3502d86a97
dazmag <iscope09@hotmail.com>
2020-09-29 21:05:26 -0400

    debugging

--- app.js
+++ app.js
@@ -1,13 +1,12 @@
 // GAME RULES;
-// Part 1
-// -must be able to determine time
-// -MUST be able to count the words 
-// -Must be able to display score for how fast they type
-
+// Part1√
+// -must be able to determine time√
+// -MUST be able to count the words √
+// -Must be able to display score for how fast they type√
 
 // List of Animals
 const words = ['dog', 'rabbit', 'cat', 'shark', 'seahorse', 'turtle',
-    'dolphin', 'tiger', 'bear', 'Penguin', 'ladybug', 'elephant','giraffe', 'butterfly',
+    'dolphin', 'tiger', 'bear', 'penguin', 'ladybug', 'elephant','giraffe', 'butterfly',
     'spider', 'dinosaur', 'fox', 'hamster', 'koala', 'anteater'
 ]
 
@@ -21,15 +20,26 @@ const fvWords = [ 'banana', 'carrot', 'spinach', 'orange', 'mango', 'grapes', 's
     'cabbage', 'cucumber', 'potato', 'ginger', 'avocado', 'radish',
     'mushroom', 'pepper', 'pumpkin'
 ]
-const word = document.getElementById('word');
-const text = document.getElementById('text');
-const scores = document.getElementById('score');
-const timeLimit = document.getElementById('time');
-const endGame = document.getElementById('end-game');
-const settingsBtn = document.getElementById('settings-btn');
+
+
 const settings = document.getElementById('settings');
-const form = document.getElementById('form');
-const diffSelect = document.getElementById('Difficulty');
+
+const diffSelect = document.getElementById('difficulty');
+let difficulty = localStorage.getItem('difficulty') !== null ?
+localStorage.getItem('difficulty') : 'medium';
+
+diffSelect.value =
+localStorage.getItem('difficulty') !== null ?
+localStorage.getItem('difficulty') : 'medium';
+
+const gameType = document.getElementById('game');
+let game = localStorage.getItem('game') !== null ?
+localStorage.getItem('game') : 'planets';
+
+gameType.value = 
+localStorage.getItem('game') !== null ?
+localStorage.getItem('game') : 'planets';
+
 // Variable for random word
 let randomWord;
 
@@ -37,26 +47,100 @@ let randomWord;
 let score = 0;
 
 //Variable for time
-let time = 10;
+let time = 11;
 
-//function to return a randomWord
+//function to return a random Word
 const randomWords = () => {
-    return words[Math.floor(Math.random() * words.length)];
-} 
+    return words[Math.floor(Math.random() * words.length)];    
+}
+//function to return a random Planet
+const randomPlanet = () => {
+    return moonWords[Math.floor(Math.random() * moonWords.length)];    
+}
+//function to return a random fruit or veggie
+const randomFruits = () => {
+    return fvWords[Math.floor(Math.random() * fvWords.length)];    
+}
+
+const endGame = document.getElementById('end-game-container');
+//function to finsih and reset game
+const finish = () => {
+    if(score <= 4) {
+        endGame.innerHTML = `
+        <h1>You lost your rabbit</h1>
+        <p>Your final score is ${score}</p>
+        <img src='https://i.imgur.com/XmdVia6.gif' height= '80%'>
+        <button onclick='location.reload()'>Reload</button>
+        `; 
+    }
+    else if(score > 4 || score <= 8) {
+        endGame.innerHTML = `
+        <h1>You an All Star</h1>
+        <p>Your final score is ${score}</p>
+        <img src='https://p0.pikist.com/photos/929/976/dog-laptop-computer-glasses-spectacles-office-desk-pet-business.jpg' width= '40%' height= '60%'>
+        <button onclick='location.reload()'>Reload</button>
+        `; 
+    } else {
+        `<h1>You the Best</h1>
+        <p>Your final score is ${score}</p>
+        <img src=''>
+        <button onclick='location.reload()'>Reload</button>
+        `;
+    }
+    endGame.style.display = 'flex';
+}
+
+const startGame = () => {
+    document.getElementById('start').addEventListener('click', () => {
+//Timer
+//setInterval allows user to run a specific function
+//In this case i need my time to be updated
+    const timeLimit = document.getElementById('time');
+    const updateTime = () => {
+        time--;
+        timeLimit.innerHTML = time + 's';
+        //the time is running pass 0 so i need a condition to start and reset the time
+        if(time === 0) {
+            clearInterval(timeInterval);
+            finish();
+        }
+    }
+    const timeInterval = setInterval(updateTime, 1100);
+
+    })
+    
+}
+startGame()
+
 // select word
+const word = document.getElementById('word');
 const getWord = () => {
+    if(game === 'animals') {
     randomWord = randomWords();
     word.innerHTML = randomWord;
+    } else if(game === 'planets') {
+    randomWord = randomPlanet();
+    word.innerHTML = randomWord; 
+    } else if(game === 'fruits') {
+    randomWord = fvWords();
+    word.innerHTML = randomWord;
+    }
 }
 getWord()
 
+// select Planet
+ 
+
 //Function to update score
+const scores = document.getElementById('score');
 const addScore = () => {
     score++;
     scores.innerHTML = score;
 }
 
+//Function to update time
 
+const text = document.getElementById('text');
 text.addEventListener('input', (e) => {
     const insertedText = e.target.value;
     if(insertedText === randomWord) {
@@ -64,10 +148,29 @@ text.addEventListener('input', (e) => {
         addScore();
     //Need to clear the text after it has been written to continue playing
     e.target.value = '';
+    if(difficulty === 'hard') {
+        time += 2;
+    } else if(difficulty === 'meduim') {
+        time +=3
+    } else {
+        time += 5;
+    }
+
+    
     }
 })
 
+//
+const form = document.getElementById('settings-form');
+form.addEventListener('change', (e) => {
+    difficulty = e.target.value;
+    localStorage.setItem('difficulty', difficulty);
+});
 
- 
+const newArray = document.getElementById('differentarrays');
+form.addEventListener('change', (e) => {
+    game = e.target.value;
+    localStorage.setItem('game', game);
+});
 
 
